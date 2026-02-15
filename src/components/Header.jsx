7e13fb8a248963260a83a3a3e14ae72f6088b9fc
Stephen Burggraaf <steedsranch@gmail.com>
2026-02-15 13:49:46 -0800

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logo} alt="Snugglebunz Logo" className="logo" />
+      <h1 className="title">Snugglebunz</h1>
+    </header>
+  );
+}
+
+export default Header;
