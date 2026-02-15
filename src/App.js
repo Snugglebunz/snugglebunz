beb406dde16d4168da0ad470c06f704c94ec610f
Stephen Burggraaf <steedsranch@gmail.com>
2026-02-15 14:00:04 -0800

    video added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Header from "./components/Header";
+import heroVideo from "./assets/hero-video.mp4";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <main className="main">
+        <video
+          className="centerVideo"
+          src={heroVideo}
+          autoPlay
+          loop
+          muted
+          playsInline
+          controls={false}
+        />
+      </main>
     </div>
   );
 }
