13a94e1d540cba6348a9c8a1ad8c5559bf232c92
Stephen Burggraaf <steedsranch@gmail.com>
2026-02-16 13:09:07 -0800

    remove header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import "./Header.css";
 import logo from "../assets/logo.png";
-import { ReactComponent as BigS } from "../assets/S.svg";
 
 function Header() {
   return (
     <header className="header">
       <img src={logo} alt="Snugglebunz Logo" className="logo" />
-
-      <h1 className="title">
-        <BigS className="bigSLogo" aria-label="S" />
-        <span className="restText">nugglebunz</span>
-      </h1>
     </header>
   );
 }
