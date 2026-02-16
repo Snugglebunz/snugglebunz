a75709aa85c89a08ee6bd8541cc606455fc109db
Stephen Burggraaf <steedsranch@gmail.com>
2026-02-16 10:03:59 -0800

    added s style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Header.css";
 import logo from "../assets/logo.png";
+import BigS from "../assets/S.svg";
 
 function Header() {
   return (
     <header className="header">
-        <img src={logo} alt="Snugglebunz Logo" className="logo" />
-        <h1 className="title">
-            <span className="bigS">S</span>nugglebunz
-        </h1>    
+      <img src={logo} alt="Snugglebunz Logo" className="logo" />
+
+      <h1 className="title">
+        <img src={BigS} alt="S" className="bigSLogo" />
+        <span className="restText">nugglebunz</span>
+      </h1>
     </header>
   );
 }
